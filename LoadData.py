1daffe673343de4d286f0353dfd0c640ad7ff3f3
PRCinguhou <pingruchou1125tw@gmail.com>
2019-06-08 00:59:55 +0800

    add LoadData

---
+++ LoadData.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+from os.path import join
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torch 
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+from torchvision import transforms
+from os import listdir
+import pandas as pd
+
+mean, std = np.array([0.5, 0.5, 0.5]), np.array([0.5, 0.5, 0.5])
+
+transform = transforms.Compose([
+	transforms.Resize((28, 28)),
+	transforms.ToTensor(),
+	transforms.Normalize(mean, std)
+	])
+
+
+class DATASET(Dataset):
+
+	def __init__(self, img_path, label_path):
+		super(DATASET, self).__init__()
+
+		self.img_path = join(os.getcwd(), img_path)
+		self.label_path = join(os.getcwd(), label_path)
+		self.label_numpy = pd.read_csv(self.label_path).values[:, 1]
+		self.imgs_files = listdir(self.img_path)
+		
+	
+	def __len__(self):
+		return len(self.imgs_files)
+
+
+	def __getitem__(self, idx):
+		
+		filename = self.imgs_files[idx]
+		img = transform(Image.open(join(self.img_path, filename)))
+		
+		label = self.label_numpy[idx]
+		label = torch.LongTensor([label])
+
+		return img, label
+
+
+	
